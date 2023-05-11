e02a3c98ab5adcd77139fd58adc7557698c9f0b7
Irfanhuda <irfanhudaa@gmail.com>
2023-05-11 16:49:53 +0700

    menambah nim dan nama

--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@ import pengguna.*;
 class Main {
     public static void main(String[] args) {
         Mhs mhs = new Mhs("ini_username", "ini_password", "Mochhamad Idris", "A123", 3);
-System.out.println(mhs.getNama()); 
-System.out.println(mhs.getNim()); 
-System.out.println(mhs.getSmt());
+System.out.println("Nama Mhs:"+mhs.getNama()); 
+System.out.println("Nim Mhs:"+mhs.getNim()); 
+System.out.println("Smt Mhs:"+mhs.getSmt());
+
+//System.out.println("Username : ini username");
+//System.out.println("Password : ini password");
+//maaf Pakusername dan password belum memiliki fungsi getter 
+ // Irfanhuda Arkananta. S G.111.19.0032
  
 }
 }
